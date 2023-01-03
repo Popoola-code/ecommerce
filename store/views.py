ecfa84ad88aa3ea0c7b74dba20a4851246013116
popoolacode <peterimpact451@gmail.com>
2023-01-03 06:54:16 +0100

    first commit

---
+++ store/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def store(request):
+    content = {}
+    return render(request, 'store/store.html', content)
+
+def cart(request):
+    content = {}
+    return render(request, 'store/cart.html', content)
+
+def checkout(request):
+    content = {}
+    return render(request, 'store/checkout.html', content)
